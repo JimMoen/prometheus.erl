96dd940958fe33552e9387299fc708cc39e1ad29
x1001100011 <x1001100011@gmail.com>
2021-06-23 21:33:42 -0700

    fix: higher level elixir warning

--- mix.exs
+++ mix.exs
@@ -2,10 +2,12 @@ defmodule Prometheus.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :prometheus,
-     version: "3.1.0",
-     description: description,
-     package: package]
+    [
+      app: :prometheus,
+      version: "3.1.0",
+      description: description(),
+      package: package()
+    ]
   end
 
   defp description do
@@ -15,15 +17,19 @@ defmodule Prometheus.Mixfile do
   end
 
   defp package do
-    [build_tools: ["rebar3"],
-     maintainers: ["Ilya Khaprov"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/deadtrickster/prometheus.erl",
-              "Prometheus.ex" => "https://hex.pm/packages/prometheus_ex",
-              "Ecto Instrumenter" => "https://hex.pm/packages/prometheus_ecto",
-              "Phoenix Instrumenter" => "https://hex.pm/packages/prometheus_phoenix",
-              "Plugs Instrumenter/Exporter" => "https://hex.pm/packages/prometheus_plugs",
-              "Process info Collector" => "https://hex.pm/packages/prometheus_process_collector"},
-     files: ["bin", "src", "include", "README.md", "LICENSE", "rebar.config"]]
+    [
+      build_tools: ["rebar3"],
+      maintainers: ["Ilya Khaprov"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/deadtrickster/prometheus.erl",
+        "Prometheus.ex" => "https://hex.pm/packages/prometheus_ex",
+        "Ecto Instrumenter" => "https://hex.pm/packages/prometheus_ecto",
+        "Phoenix Instrumenter" => "https://hex.pm/packages/prometheus_phoenix",
+        "Plugs Instrumenter/Exporter" => "https://hex.pm/packages/prometheus_plugs",
+        "Process info Collector" => "https://hex.pm/packages/prometheus_process_collector"
+      },
+      files: ["bin", "src", "include", "README.md", "LICENSE", "rebar.config"]
+    ]
   end
 end
